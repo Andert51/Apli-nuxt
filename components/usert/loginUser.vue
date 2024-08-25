8c13085e4edaabf59050369116dd0eb587e8c4b5
Andert51 <andrest05143@gmail.com>
2024-08-24 20:01:25 -0600

    LogiCard Barely Finished

---
+++ components/usert/loginUser.vue
@@ -0,0 +1,13 @@
+<template>
+  <v-card color="#00BCD4">
+    <v-card-title>
+      Login
+    </v-card-title>
+    <v-card-text>
+      Opciones
+    </v-card-text>
+    <v-card-actions>
+      Botones
+    </v-card-actions>
+  </v-card>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <v-row justify="center" align="center">
+    <LoginUser />
+  </v-row>
+</template>
+
+<script>
+import LoginUser from '~/components/usert/loginUser.vue'
+
+export default {
+  name: 'IndexPage',
+  components: {
+    LoginUser
+  },
+  layout: 'login'
+}
+</script>
